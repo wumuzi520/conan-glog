86671c7c38667105f7fd769a085577d298483a4d
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2017-11-21 19:05:20 +0100

    Should not mix static and dynamic runtimes

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,6 +8,24 @@ class GlogTestConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.verbose = True
+        compiler = str(self.settings.compiler)
+        flags = []
+
+        if compiler in ("gcc", "clang", "apple-clang"):
+            if self.settings.arch == 'x86':
+                flags.append("-m32")
+            else:
+                flags.append("-m64")
+
+        self.output.info(
+            "arch: {0}; flags: {1}; os: {2}; compiler: {3}".format(self.settings.arch, flags, self.settings.os,
+                                                                   compiler))
+
+        if compiler in ("Visual Studio"):
+            pass
+        cmake.definitions["CMAKE_C_FLAGS"] = " ".join(flags)
+        cmake.definitions["CMAKE_CXX_FLAGS"] = cmake.definitions["CMAKE_C_FLAGS"]
+
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
         cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
         cmake.build()
