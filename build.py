48705a0103f646fb9db83c8e43520390ca4bba53
Jerry W <jerrywiltse@gmail.com>
2018-01-30 15:46:04 -0500

    add gflags shared in build.py

--- build.py
+++ build.py
@@ -4,8 +4,16 @@
 
 from bincrafters import build_template_default
 
+def add_gflags_shared(build):
+    if build_shared.is_shared():
+        shared_option_name = "%s:shared" % build_shared.get_name_from_recipe()
+        build.options.update({'gflags:shared' : build.options[shared_option_name]})
+    return build
+    
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    
+    builder.builds = map(add_boost_shared, builder.items)
 
     builder.run()
