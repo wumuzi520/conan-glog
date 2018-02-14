69a5cb6a925cd85ecf8256d4fc0674c75dcfda84
Jerry W <jerrywiltse@gmail.com>
2018-02-14 09:38:58 -0500

    switch gflags dep to conan/stable

--- conanfile.py
+++ conanfile.py
@@ -24,7 +24,7 @@ class GlogConan(ConanFile):
             self.options.remove("fPIC")
 
         if self.options.with_gflags:
-            self.requires("gflags/2.2@bincrafters/stable")
+            self.requires("gflags/2.2.1@conan/stable")
         
 
     def source(self):
